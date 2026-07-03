14c1ba084921a9eb98a0e62b251de2191f1fe6de
Almog Yaacov <youremail@example.com>
2026-07-04 00:54:36 +0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,4 +110,9 @@ dependencies {
 // ViewModel + LiveData (MVVM)
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata:2.7.0")
+
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.7.0")
 }
