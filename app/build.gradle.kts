1c64d9497e36feab02082957b2c899c031e51586
Almog Yaacov <youremail@example.com>
2026-07-04 23:32:44 +0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.mogomarket.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.4"
+        versionCode = 10
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
